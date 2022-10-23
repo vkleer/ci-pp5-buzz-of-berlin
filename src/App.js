d2f8a77ba260fa7934f4919930ad5212e47181a9
Vilayat Kleer <vkleer@hotmail.com>
2022-10-23 14:31:25 +0000

    Create Main class in App.module.css, import Container in App.js and apply Main class to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import styles from  './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home</h1>
+        <h1>Log in</h1>
+        <h1>Create account</h1>
+      </Container>
     </div>
   );
 }
