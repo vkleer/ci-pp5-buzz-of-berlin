41ab0d834ef7ccaa3db03e2f843c72f0286a5a3d
Vilayat Kleer <vkleer@hotmail.com>
2022-10-23 13:05:44 +0000

    Add basic styles to App.module.css, import App.module.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import styles from  './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
