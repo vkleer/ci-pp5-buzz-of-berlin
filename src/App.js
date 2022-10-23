2397f7b668ac38ece4ef35c15f4c6dc7ad167e79
Vilayat Kleer <vkleer@hotmail.com>
2022-10-23 18:40:56 +0000

    Import SignUpForm component and set signup Route to render SignUpForm component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={()=> <h1>Home</h1>} />
           <Route exact path="/login" render={()=> <h1>Log in</h1>} />
-          <Route exact path="/signup" render={()=> <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={()=> <SignUpForm />} />
           <Route render={()=> <h1>Page not found.</h1>} />
         </Switch>
       </Container>
