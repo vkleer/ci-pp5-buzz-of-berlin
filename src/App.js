413db06c552412590d04c34b3eea56eb9567023e
Vilayat Kleer <vkleer@hotmail.com>
2022-10-23 15:09:04 +0000

    Set up Route and Switch components in App.js, set up NavLink components in NavBar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import styles from  './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home</h1>
-        <h1>Log in</h1>
-        <h1>Create account</h1>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home</h1>} />
+          <Route exact path="/login" render={()=> <h1>Log in</h1>} />
+          <Route exact path="/signup" render={()=> <h1>Sign up</h1>} />
+        </Switch>
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,30 +4,33 @@ import {Navbar} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
 import {Nav} from 'react-bootstrap';
 import logo from '../assets/logo.png';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-            <Navbar.Brand>
-                <img src={logo} alt="Logo" height="40" />
-                <span className={styles.LogoText}>Buzz of Berlin</span>
-            </Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand>
+                    <img src={logo} alt="Logo" height="40" />
+                    <span className={styles.LogoText}>Buzz of Berlin</span>
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link>
+                    <NavLink to ="/">
                         <i class="fa-solid fa-house"></i>
                         Home
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink to="/login">
                         <i class="fa-solid fa-key"></i>
                         Log in
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink to="/signup">
                         <i class="fa-solid fa-user-plus"></i>
-                        Create account
-                    </Nav.Link>
+                        Sign up
+                    </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>
