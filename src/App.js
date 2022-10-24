22c9de26c505e47856e35f633fc34c0eda42a1d6
Vilayat Kleer <vkleer@hotmail.com>
2022-10-24 14:58:40 +0000

    Import LogInForm component in App.js, set Route for login page to LogInForm component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
+import LogInForm from './pages/auth/LogInForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={()=> <h1>Home</h1>} />
-          <Route exact path="/login" render={()=> <h1>Log in</h1>} />
+          <Route exact path="/login" render={()=> <LogInForm />} />
           <Route exact path="/signup" render={()=> <SignUpForm />} />
           <Route render={()=> <h1>Page not found.</h1>} />
         </Switch>
