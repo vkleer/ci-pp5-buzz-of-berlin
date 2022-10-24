121a1be79fafc256932fc3a725fc96dba46a1295
Vilayat Kleer <vkleer@hotmail.com>
2022-10-24 15:39:47 +0000

    Import UseCurrentUser object, change NavLinks in Navbar when user is logged in/out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import styles from '../styles/NavBar.module.css';
-import {Navbar} from 'react-bootstrap';
-import {Container} from 'react-bootstrap';
-import {Nav} from 'react-bootstrap';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
 import logo from '../assets/logo.png';
-import {NavLink} from 'react-router-dom';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 const NavBar = () => {
-  return (
+    const currentUser = useCurrentUser();
+
+    const loggedInIcons = (
+        <>
+            {currentUser?.username}
+        </>
+    );
+    const loggedOutIcons = (
+        <> 
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
+                <i className="fa-solid fa-key"></i>
+                Log in
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+                <i className="fa-solid fa-user-plus"></i>
+                Sign up
+            </NavLink>
+        </>
+    );
+
+return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <NavLink to="/">
@@ -23,14 +47,7 @@ const NavBar = () => {
                         <i className="fa-solid fa-house"></i>
                         Home
                     </NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
-                        <i className="fa-solid fa-key"></i>
-                        Log in
-                    </NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-                        <i className="fa-solid fa-user-plus"></i>
-                        Sign up
-                    </NavLink>
+                    {currentUser ? loggedInIcons : loggedOutIcons}
                 </Nav>
             </Navbar.Collapse>
         </Container>
