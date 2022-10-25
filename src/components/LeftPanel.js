c53525b4da0520dccfc0417679a41835fe900b5e
Vilayat Kleer <vkleer@hotmail.com>
2022-10-25 15:35:24 +0000

    Create LefPanel.js and LeftPanel component, create LeftPanel.module.css

---
+++ src/components/LeftPanel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+import styles from '../styles/LeftPanel.module.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+
+const LeftPanel = () => {
+    const currentUser = useCurrentUser();
+
+    const loggedInIcons = (
+        <>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
+                <i className="fa-solid fa-square-plus"></i>Create post
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
+                <i className="fa-solid fa-square-plus"></i>Create recommendation
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
+                <i className="fa-solid fa-square-plus"></i>Create event
+            </NavLink>
+        </>
+    );
+
+return (
+    <Container className="px-0">
+        <Navbar className={styles.NavBar} expand="md" fixed="left">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="flex-column text-left">
+                    {currentUser && loggedInIcons}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Container>
+  )
+}
+
+export default LeftPanel
