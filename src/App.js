7f632a8dd3c2b779b32a254923c7051e268fc174
Vilayat Kleer <vkleer@hotmail.com>
2022-10-25 15:36:21 +0000

    Import LeftPanel component into App.js, change layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import styles from  './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
+
+import NavBar from './components/NavBar';
+import LeftPanel from './components/LeftPanel';
+
 import SignUpForm from './pages/auth/SignUpForm';
 import LogInForm from './pages/auth/LogInForm';
 
 function App() {
   return (
     <div className={styles.App}>
+      {/* NavBar */}
       <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={()=> <h1>Home</h1>} />
-          <Route exact path="/login" render={()=> <LogInForm />} />
-          <Route exact path="/signup" render={()=> <SignUpForm />} />
-          <Route render={()=> <h1>Page not found.</h1>} />
-        </Switch>
-      </Container>
+      <Row className={`px-0 mx-0 ${styles.Main}`}>
+        {/* LeftPanel */}
+        <Col className={`px-0 ${styles.LeftPanel}`} md={3}>
+          <LeftPanel />
+        </Col>
+        {/* Main content */}
+        <Col className="px-0 offset-3" md={9}>
+          <Switch>
+            <Route exact path="/" render={()=> <h1>Home</h1>} />
+            <Route exact path="/login" render={()=> <LogInForm />} />
+            <Route exact path="/signup" render={()=> <SignUpForm />} />
+            <Route render={()=> <h1>Page not found.</h1>} />
+          </Switch>
+        </Col>
+      </Row>
     </div>
   );
 }
