afda017ed3bf8d9e41bb14b92911496827c35833
Vilayat Kleer <vkleer@hotmail.com>
2022-10-25 12:55:11 +0000

    Update NavBar component to display new links and icons when logged in or logged out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 import styles from '../styles/NavBar.module.css';
 
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
 import logo from '../assets/logo.png';
@@ -13,12 +12,46 @@ import { useCurrentUser } from '../contexts/CurrentUserContext';
 const NavBar = () => {
     const currentUser = useCurrentUser();
 
-    const loggedInIcons = (
+    const loggedInMainIcons = (
         <>
-            {currentUser?.username}
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/posts"
+            >
+                <i class="fa-solid fa-image"></i>Posts
+            </NavLink>
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/recommendations"
+            >
+                <i class="fa-solid fa-thumbs-up"></i>Recommendations
+            </NavLink>
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/events"
+            >
+                <i class="fa-solid fa-calendar-days"></i>Events
+            </NavLink>
+            <NavLink 
+                className={styles.NavLink} activeClassName={styles.Active} to="/liked"
+            >
+                <i className="fas fa-heart"></i>Liked
+            </NavLink>
         </>
     );
-    const loggedOutIcons = (
+    const loggedInAccIcons = (
+        <> 
+            <NavLink 
+                className={styles.NavLink} to="/" onClick={() => {}}
+            >
+                <i className="fas fa-sign-out-alt"></i>Sign out
+            </NavLink>
+        </>
+    );
+    const loggedOutAccIcons = (
         <> 
             <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
                 <i className="fa-solid fa-key"></i>
@@ -33,7 +66,6 @@ const NavBar = () => {
 
 return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
             <NavLink to="/">
                 <Navbar.Brand>
                     <img src={logo} alt="Logo" height="40" />
@@ -42,15 +74,17 @@ return (
             </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
+                <Nav className="m-md-auto ml-sm-0 text-md-center text-sm-left">
                     <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to ="/">
                         <i className="fa-solid fa-house"></i>
                         Home
                     </NavLink>
-                    {currentUser ? loggedInIcons : loggedOutIcons}
+                    {currentUser && loggedInMainIcons}
+                </Nav>
+                <Nav className="ml-sm-0 text-left">
+                    {currentUser ? loggedInAccIcons : loggedOutAccIcons}
                 </Nav>
             </Navbar.Collapse>
-        </Container>
         </Navbar>
   )
 }
