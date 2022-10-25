a4f343a7b75c328bb82d2363ce9ec1a74454b17c
Vilayat Kleer <vkleer@hotmail.com>
2022-10-25 22:55:50 +0000

    Create Dropdown for CreatePanel component on small screens, create DropDownMenu class in CreatePanel.module.css

--- src/components/CreatePanel.js
+++ src/components/CreatePanel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import styles from '../styles/CreatePanel.module.css';
 
@@ -17,27 +18,48 @@ const CreatePanel = () => {
 
     const loggedInIcons = (
         <>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
-                <i className="fa-solid fa-square-plus"></i>Create post
+            <NavLink 
+                className={`d-block ${styles.PanelLink}`} 
+                activeClassName={styles.Active} 
+                to="/posts/create"
+            >
+                <i className="fa-solid fa-square-plus"></i>
+                Create post
             </NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
-                <i className="fa-solid fa-square-plus"></i>Create recommendation
+            <NavLink 
+                className={`d-block ${styles.PanelLink}`} 
+                activeClassName={styles.Active} 
+                to="/posts/create"
+            >
+                <i className="fa-solid fa-square-plus"></i>
+                Create recommendation
             </NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
-                <i className="fa-solid fa-square-plus"></i>Create event
+            <NavLink 
+                className={`d-block ${styles.PanelLink}`} 
+                activeClassName={styles.Active} 
+                to="/posts/create"
+            >
+                <i className="fa-solid fa-square-plus"></i>
+                Create event
             </NavLink>
         </>
     );
 
 return (
-    <Container className="px-0 d-sm-none d-md-flex">
+    <Container className="px-0">
         <Navbar className={styles.NavBar} expand="md" fixed="left">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="flex-column text-left">
+            <Nav className="flex-column text-left d-none d-md-flex">
+                {currentUser && loggedInIcons}
+            </Nav>
+            <Dropdown className="d-flex mx-auto d-md-none">
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                    <i className="fa-solid fa-square-plus text-white"></i> Create
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className={styles.DropDownMenu}>
                     {currentUser && loggedInIcons}
-                </Nav>
-            </Navbar.Collapse>
+                </Dropdown.Menu>
+            </Dropdown>
         </Navbar>
     </Container>
   )
