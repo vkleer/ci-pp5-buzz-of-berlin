a3a28f8d678ad66292ff60ed2639a9342aedcf2a
Vilayat Kleer <vkleer@hotmail.com>
2022-10-27 12:42:56 +0000

    Create Profile component to render users' Avatar, username and follow/unfollow button

---
+++ src/profiles/Profile.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from '../styles/Profile.module.css';
+import btnStyles from '../styles/Button.module.css';
+
+import Button from 'react-bootstrap/Button';
+
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { Avatar } from '../components/Avatar';
+import { useSetProfileData } from '../contexts/ProfileDataContext';
+
+/**
+ * Renders the Profile component - which displays the users' profile picture
+ * using the Avatar component, username and a follow/unfollow button.
+ * The variables and logic have been created using the Moments walkthrough
+ * and have been built upon/customised.
+ */
+const Profile = (props) => {
+    const {profile, mobile, imageSize=55} = props;
+    const {id, following_id, image, owner} = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const {handleFollow, handleUnfollow} = useSetProfileData();
+
+  return (
+    <div className={`my-3 d-flex align-items-center ${mobile && 'flex-column'}`}>
+        <div>
+            <Link className='align-self-center' to={`/profiles/${id}`}>
+                <Avatar src={image} height={imageSize} />
+            </Link>
+        </div>
+        <div className={`mx-2 ${styles.WordBreak}`}>
+            <p>{owner}</p>
+        </div>
+        <div className={`text-right ${!mobile && 'ml-auto'}`}>
+            {!mobile && currentUser && !is_owner && (
+                following_id ? (
+                    <Button 
+                        className={btnStyles.Button}
+                        onClick={() => handleUnfollow(profile)}
+                    >
+                        Unfollow
+                    </Button>
+                ) : (
+                    <Button 
+                        className={btnStyles.Button}
+                        onClick={() => handleFollow(profile)}
+                    >
+                        Follow
+                    </Button>
+                )
+            )}
+        </div>
+    </div>
+  )
+};
+
+export default Profile
