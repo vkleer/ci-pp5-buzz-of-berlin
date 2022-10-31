5c5c4cda42e04c4dba4bee188598fa73023f8a2a
Vilayat Kleer <vkleer@hotmail.com>
2022-10-31 15:58:42 +0000

    Fix indentation in Post component

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -97,19 +97,19 @@ const Post = (props) => {
         }`}>
             <Card.Body>
                 <Media className="align-items-center justify-content-between">
-                <Link to={`/profiles/${profile_id}`}>
-                    <Avatar src={profile_image} height={55} />
-                    {owner}
-                </Link>
-                <div className="d-flex align-items-center">
-                    <span>{updated_date}</span>
-                    {is_owner && setPosts && (
-                        <DotsDropdown 
-                            handleEdit={handleEdit} 
-                            handleDelete={handleDelete} 
-                        />
-                    )}
-                </div>
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={55} />
+                        {owner}
+                    </Link>
+                    <div className="d-flex align-items-center">
+                        <span>{updated_date}</span>
+                        {is_owner && setPosts && (
+                            <DotsDropdown 
+                                handleEdit={handleEdit} 
+                                handleDelete={handleDelete} 
+                            />
+                        )}
+                    </div>
                 </Media>
             </Card.Body>
             <Link to={`/posts/${id}`}>
